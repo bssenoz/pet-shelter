5cbe1ffd223cf4a7ebe4bbf19137dfca5c34fdfd
bssenoz <bsenoz@hotmail.com>
2022-12-19 11:01:25 +0300

    init

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalShelter.Models
+{
+    public class Animal
+    {
+        public int animalId { get; set; }
+        public string familya { get; set; }
+        public string species { get; set; }
+        public string images { get; set; }
+        public int age { get; set; }
+    }
+}
