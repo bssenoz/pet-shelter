281c6a5cc51a90806edda0d6561aa7716a2e9f95
bssenoz <bsenoz@hotmail.com>
2022-12-21 13:55:11 +0300

    edit and delete

--- AnimalShelter/Controllers/AnimalController.cs
+++ AnimalShelter/Controllers/AnimalController.cs
@@ -17,6 +17,62 @@ namespace AnimalShelter.Controllers
             var animals = k.Animals;
             return View(animals);
         }
+       
+        [HttpPost]
+        public IActionResult Edit(int? id, Animal a)
+        {
+            if (id != a.AnimalId)
+            {
+                TempData["hata"] = "Güncelleme Yapılmaz";
+                return View("Hata");
+            }
+            if (ModelState.IsValid)
+            {
+                k.Animals.Update(a);
+                k.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            //TempData["hata"] = "Lütfen verileri eksiksiz girin";
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id is null)
+            {
+                TempData["hata"] = "Düzenleme kısmı çalışamaz";
+                return View("Hata");
+            }
+            var a = k.Animals.FirstOrDefault(x => x.AnimalId == id);
+            if (a is null)
+            {
+                TempData["hata"] = "Düzenlenece herhangi bir yazar yok";
+                return View("Hata");
+
+            }
+            return View(a);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id is null)
+            {
+                TempData["hata"] = "Silme kısmı çalışamaz";
+                return View("Hata");
+            }
+            var y = k.Animals.FirstOrDefault(x => x.AnimalId == id);
+            if (y is null)
+            {
+                TempData["hata"] = "Silinecek herhangi bir yazar yok";
+                return View("Hata");
+
+            }
+
+            k.Animals.Remove(y);
+            k.SaveChanges();
+            //TempData["msj"] = y.YazarAd + " adlı yazar silindi";
+            return RedirectToAction("Index");
+
+
+        }
         public IActionResult Create()
         {
             return View();
@@ -24,21 +80,20 @@ namespace AnimalShelter.Controllers
         [HttpPost]
         public IActionResult Create(Animal a)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                //a.DateProperty = DateTime.Now;
                 k.Add(a);
                 k.SaveChanges();
                 return RedirectToAction("Index");
 
-            } else
+            }
+            else
             {
                 TempData["hata"] = "Lütfen Gerekli alanları doldurunuz";
                 return RedirectToAction("Create");
             }
 
         }
-
 
     }
 }
